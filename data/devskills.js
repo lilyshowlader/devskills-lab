8fbdea143890010a130a8c16915910349af912c3
lilyshowlader <lilyshowlader@gmail.com>
2022-09-20 18:13:59 -0400

    lab set up

---
+++ data/devskills.js
@@ -0,0 +1,9 @@
+const devskills = [
+  {skill: 'Javascript', level: 'struggle', _id:111 },
+  {skill: 'Node', level: 'struggle', _id:123},
+  {skill: 'Express', level: 'struggle', _id:222}
+]
+
+export {
+  devskills
+}
