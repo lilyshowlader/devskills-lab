fe965cfb8597daabb739014930f6aaddfe13bdc6
lilyshowlader <lilyshowlader@gmail.com>
2022-09-21 19:09:05 -0400

    lab complete

--- controllers/devskills.js
+++ controllers/devskills.js
@@ -1,6 +1,7 @@
 import { Devskill } from '../models/devskill.js'
 // importing from model
 
+// the function below displays all the skills http://localhost:3000/devskills
 function index(req, res) {
   Devskill.find({})
   .then(devskills => { // todos represents the result of the query, in this case ALL todos
@@ -14,12 +15,51 @@ function index(req, res) {
   })
 }
 
-// below is the newdevskills function which will allow users to add a new skill
+// below is the newdevskills function which display the screen that allows the user to add a skill
 function newDevSkill(req, res) {
   res.render('devskills/new')
 }
 
+
+// the function below using mongoose method model.create or Devskill.create allows the user to create & post the new skill
+// devskills.create is creating the skill
+// the devskill on line 30, represents the newly created dev skill.
+
+function create(req, res) {
+  console.log(req.body)
+  Devskill.create(req.body)
+  .then(devskill => {
+		// Notice we are doing a redirect here!
+    res.redirect('/devskills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/devskills')
+  })
+}
+
+// this function shows the details of each individual skill. 
+function show(req, res) {
+  Devskill.findById(req.params.id)
+  .then(devskill => {
+    res.render('devskills/show', {
+      devskill: devskill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/devskills')
+  })
+}
+
+
+
 export {
 	index,
-  newDevSkill as new
+  newDevSkill as new,
+  create,
+  show
 }
+
+// notice how each of these functions have been imported. 
+
--- routes/devskills.js
+++ routes/devskills.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import * as devskillsCtrl from '../controllers/devskills.js'
+// I want to import all as devSkillsController/I want to import all of it using this name ^^ 
 
 const router = Router()
 
@@ -8,6 +9,12 @@ router.get('/', devskillsCtrl.index)
 
 // add a new route for new devskills
 router.get('/new', devskillsCtrl.new)
+
+//  the route below goes to a specific skill using its id 
+router.get('/:id', devskillsCtrl.show)
+
+// the route below will create a new post (skill)
+router.post('/', devskillsCtrl.create)
 
 export {
   router
